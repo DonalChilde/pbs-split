851ff3b09ef58585b9bf0dbf7f15432dfd084bf2
Chad Lowe <pfmsoft@gmail.com>
2024-11-26 06:28:51 -0700

    refactor for clarity, add doc strings

--- src/pbs_split/cli/pages_cli.py
+++ src/pbs_split/cli/pages_cli.py
@@ -46,10 +46,10 @@ def split(
     path_in: Annotated[
         Path,
         typer.Argument(
-            help="Text file with pages, or a directory containing the text files.",
+            help="The bid package text file.",
             exists=True,
             file_okay=True,
-            dir_okay=True,
+            dir_okay=False,
         ),
     ],
     path_out: Annotated[
@@ -63,30 +63,93 @@ def split(
 ):
     """Split the text version of a PBS pairing package into pages."""
     _ = ctx
-    jobs = collect_jobs(path_in=path_in, path_out=path_out, overwrite=overwrite)
+    job = build_job_from_file(path_in=path_in, path_out=path_out, overwrite=overwrite)
+    jobs = [job]
     extract_pages_rich(jobs=jobs)
 
 
-def collect_jobs(
+@app.command()
+def split_all(
+    ctx: typer.Context,
+    path_in: Annotated[
+        Path,
+        typer.Argument(
+            help="A directory containing the bid package text files.",
+            exists=True,
+            dir_okay=True,
+            file_okay=False,
+        ),
+    ],
+    path_out: Annotated[
+        Path,
+        typer.Argument(help="The output directory."),
+    ],
+    overwrite: Annotated[
+        bool,
+        typer.Option(help="Allow overwriting output files."),
+    ] = False,
+):
+    """Split the text version of a PBS pairing package into pages.
+
+    Files will be output to PATH_OUT/PATH_IN.stem/pages/
+    """
+    _ = ctx
+    jobs = build_jobs_from_dir(path_in=path_in, path_out=path_out, overwrite=overwrite)
+    extract_pages_rich(jobs=jobs)
+
+
+def build_job_from_file(path_in: Path, path_out: Path, overwrite: bool) -> SplitPageJob:
+    """Check for valid inputs and return a `SplitPageJob`."""
+    if path_in.is_file():
+        if path_in.suffix.lower() != ".txt":
+            raise typer.echo(
+                f"Input path might not be a valid file, it does not have a .txt suffix. {path_in}"
+            )
+    else:
+        raise typer.BadParameter(f"Input path is not a valid file. {path_in}")
+    if path_out.is_file():
+        raise typer.BadParameter(
+            f"Output path is a file, it should be a directory. {path_out}"
+        )
+    job = SplitPageJob(path_in=path_in, path_out=path_out, overwrite=overwrite)
+    return job
+
+
+def build_jobs_from_dir(
     path_in: Path, path_out: Path, overwrite: bool
 ) -> Sequence[SplitPageJob]:
-    """Build a collections of jobs to do."""
-    if path_in.is_dir():
-        files = [f for f in path_in.glob(".txt", case_sensitive=False) if f.is_file()]
-    elif path_in.is_file():
-        files = [path_in]
-    else:
-        raise typer.BadParameter(
-            "Input path is not a valid file, or directory containing valid files."
-        )
+    """Collect text files, and use to build `SplitPageJob`s."""
+    if not path_in.is_dir():
+        raise typer.BadParameter("PATH_IN should be a directory.")
+    files = [f for f in path_in.glob(".txt", case_sensitive=False) if f.is_file()]
     jobs: list[SplitPageJob] = []
     for file in files:
-        if len(files) > 1:
-            dest_dir = path_out / Path(file.stem) / Path("pages")
-        else:
-            dest_dir = path_out
-        jobs.append(SplitPageJob(path_in=file, path_out=dest_dir, overwrite=overwrite))
+        out_path = file.parent / file.stem / "pages"
+        job = build_job_from_file(path_in=file, path_out=out_path, overwrite=overwrite)
+        jobs.append(job)
     return jobs
+
+
+# def collect_jobs(
+#     path_in: Path, path_out: Path, overwrite: bool
+# ) -> Sequence[SplitPageJob]:
+#     """Build a collections of jobs to do."""
+#     if path_in.is_dir():
+#         files = [f for f in path_in.glob(".txt", case_sensitive=False) if f.is_file()]
+#     elif path_in.is_file():
+#         files = [path_in]
+#     else:
+#         raise typer.BadParameter(
+#             "Input path is not a valid file, or directory containing valid files."
+#         )
+#     jobs: list[SplitPageJob] = []
+#     for file in files:
+#         if len(files) > 1:
+#             dest_dir = path_out / Path(file.stem) / Path("pages")
+#         else:
+#             dest_dir = path_out
+#         jobs.append(SplitPageJob(path_in=file, path_out=dest_dir, overwrite=overwrite))
+#     return jobs
 
 
 def extract_pages_rich(jobs: Sequence[SplitPageJob]):
--- src/pbs_split/extract_pages.py
+++ src/pbs_split/extract_pages.py
@@ -1,6 +1,7 @@
+"""This module handles splitting a bid package text file into `PageLines`."""
+
+from collections.abc import Iterable, Iterator
 from pathlib import Path
-from typing import Iterable, Iterator, List
-from uuid import uuid4
 
 from pfmsoft.indexed_string.index_strings import index_lines_in_file
 from pfmsoft.indexed_string.model import IndexedString
@@ -8,11 +9,18 @@ from pfmsoft.indexed_string.model import IndexedString
 from pbs_split.models import PageLines, page_lines_serializer
 
 
-def package_to_lines_of_pages(
+def split_package_to_lines_in_page(
     lines: Iterable[IndexedString],
-) -> Iterator[List[IndexedString]]:
-    accumulated_lines: List[IndexedString] = []
+) -> Iterator[list[IndexedString]]:
+    """Collect the lines in each page.
 
+    Args:
+        lines: The lines from a bid package.
+
+    Yields:
+        The lines in each page.
+    """
+    accumulated_lines: list[IndexedString] = []
     is_page = False
     for indexed_line in lines:
         if is_page:
@@ -30,12 +38,21 @@ def package_to_lines_of_pages(
 
 
 def parse_pages_from_file(path_in: Path) -> Iterator[PageLines]:
+    """Get the `PageLines` from a bid package text file.
+
+    Args:
+        path_in: The path to a bid package text file.
+
+    Yields:
+        The `PageLines`
+    """
     reader = index_lines_in_file(file_path=path_in, index_start=1)
     yield from parse_pages(lines=reader)
 
 
 def parse_pages(lines: Iterator[IndexedString]) -> Iterator[PageLines]:
-    for idx, lines_of_page in enumerate(package_to_lines_of_pages(lines), start=1):
+    """Get the `PageLines` from a collection of `IndexedString`s."""
+    for idx, lines_of_page in enumerate(split_package_to_lines_in_page(lines), start=1):
         page = PageLines(idx=idx, lines=lines_of_page)
         yield page
 
@@ -43,13 +60,14 @@ def parse_pages(lines: Iterator[IndexedString]) -> Iterator[PageLines]:
 def write_pages(
     file_stem: str, pages: Iterator[PageLines], path_out: Path, overwrite: bool
 ) -> int:
+    """Write the `PageLines` to file with a default file name."""
     pages_list = list(pages)
     count = 0
+    serializer = page_lines_serializer()
     for idx, page in enumerate(pages_list, start=1):
         result_path = path_out / Path(
             f"{file_stem}.page_{idx}_of_{len(pages_list)}.json"
         )
-        serializer = page_lines_serializer()
         serializer.save_as_json(
             path_out=result_path, complex_obj=page, overwrite=overwrite
         )
