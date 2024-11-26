f351e351d14cb59342e69995f9bb3a6f25ac70d2
Chad Lowe <pfmsoft@gmail.com>
2024-11-26 14:20:40 -0700

    update tests for splitting pages to trips.

--- tests/pbs_split/cli/test_pages_to_trips_split.py
+++ tests/pbs_split/cli/test_pages_to_trips_split.py
@@ -1,22 +1,27 @@
+"""Test using the cli to split pages into trips."""
+
 from importlib import resources
 from pathlib import Path
 
-from tests.resources import RESOURCES_ANCHOR
 from typer.testing import CliRunner
 
 from pbs_split.cli.main_typer import app
+from tests.resources import RESOURCES_ANCHOR
 
 DATA_FILE_PATH = "page"
 
 
-def test_split_pages_to_trips(runner: CliRunner, test_output_dir: Path, capsys):
+def test_split_pages_to_trips(runner: CliRunner, test_output_dir: Path, capsys):  # type: ignore
+    """Test the cli - convert a directory of pages to trips."""
     file_resource = resources.files(RESOURCES_ANCHOR).joinpath(DATA_FILE_PATH)
     path_out = test_output_dir / "cli" / "pages_to_trips"
     with resources.as_file(file_resource) as input_path:
-        result = runner.invoke(app, ["trips", "split", str(input_path), str(path_out)])
+        result = runner.invoke(
+            app, ["trips", "split-all", str(input_path), str(path_out)]
+        )
         if result.stderr_bytes is not None:
             print(result.stderr)
-        with capsys.disabled():
+        with capsys.disabled():  # type: ignore
             print(result.stdout)
         assert result.exit_code == 0
         assert "41 trips found" in result.stdout
